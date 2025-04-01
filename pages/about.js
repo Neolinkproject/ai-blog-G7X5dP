75cbaf094d8078f9213893d1518cfb0792e67a9e
Neolinkproject <github@austinlambe.com>
2025-04-01 11:18:12 +0100

    testx

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function About() {
+return (
+<div>
+<Head>
+<title>About Us</title>
+<meta name='description' content='Learn more about us' />
+</Head>
+<h1>About Us</h1>
+<p>We are a top-notch tech company focused on delivering quality products.</p>
+</div>
+);
+}
